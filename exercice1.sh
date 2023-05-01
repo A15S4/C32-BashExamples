ef7357113ca9080b63bd0a0e2bb1b9823d588176
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 11:58:42 -0400

    Création exercice1

--- exercice1.sh
+++ exercice1.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-# La ligne suivante permet de v�rifier si un mot existe dans le dictionnaire
-# Il faut suffixer cet URL avec le mot entr� par l'uasger (ex: word=chat)
+# La ligne suivante permet de v�rifier si un mot existe dans le dictionnaire
+# Il faut suffixer cet URL avec le mot entr� par l'uasger (ex: word=chat)
 #wget -qO - http://dictionary.objectif8.com/exists.php?word=
+
+#1 demander à l'usager un mot
+
+read -p "Entrez un mot : " mot
+#autre facon du read: echo -n "Entrez un mot : "
+# read mot
+
+#2 vérifier si le mot existe à l'aide de l'url
+
+resultat=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$mot`
+
+
+#3 afficher à l' écran si le mot existe, ou pasthen 
+if [[ $resultat = 1 ]]
+then
+    echo "Le mot existe"
+else
+    echo "Le mot n'existe pas"
+fi
