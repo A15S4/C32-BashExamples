14d7ad09830ec2d603ca9cbd99152e5cddd40569
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 11:06:12 -0400

    Création 09-fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# -e = fichier existe
+# -f = c'est un fichier ?
+# -d = c'est un dossier ?
+
+
+# va confirmer si le nom du paramètre correspond à un fichier qui existe
+if [[ -e $1 ]]
+then
+    echo " le fichier existe"
+fi
