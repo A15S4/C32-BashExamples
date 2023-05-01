84d3d38f32a3cbb097a08ac7eb6968f0993674a2
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 10:44:24 -0400

    Création 05-boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+
+#PREMIER TYPE DE BOUCLES
+for ((i = 0; i < 5; i++))
+do
+    echo $i
+done
+
+
+#DEUXIEME TYPE DE BOUCLE
+
+# `` = exécution d'une commande et met le résultat dans la variable
+liste=`ls` #accents graves
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+# TROISIEME TYPE DE BOUCLES
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
