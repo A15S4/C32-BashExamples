d53f5dd013fd94a52136b3b5e1a046ef7724b552
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 09:56:52 -0400

    Création 02-Variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#pour affecter une variable, il ne faut PAS mettre d'espaces
+prenom=Aissa
+
+echo $prenom
+
+# declare rarement utilisé
+declare -i age=33
+# -r donne une constante
+declare -r CHEMIN=/bin
+
+# donne le premier script
+echo "Nom script" $0
+
+#  $# veut dire combien on a passé de paramètres
+echo "Nombre params" $#
+
+
