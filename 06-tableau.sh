6bbf91aeb60d04ae1de2b615e9b5383867f24d0f
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 10:47:50 -0400

    Création 06-tableau

--- 06-tableau.sh
+++ 06-tableau.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#tableau est utilisé avec -a
+# à l'indice 0: The 
+# à l'indice 1: Matrix
+declare -a film=(The Matrix)
+echo $(film[0])
+echo $(film[1])
+echo "Nombre d'éléments: " $(film[@])
