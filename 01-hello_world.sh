bd2dee92c074b0a24f08ecdf8822d37b5edb881e
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 09:46:00 -0400

    Création 01-hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+
+# wasfasdf
+echo "Hello world" $USER
+# exit 
+
+echo -n "Vive "
+echo "Linux"
+
+# -e permet d'interpréter les retours de la ligne et etc (\n, \t,...)
+echo -e "1\n2\n3"
+
+#poser question  a un usager
+read -p "Quel age avez-vous?" age
+
+# il faut metre le signe de dollar avant d'appeler la variable
+echo $age "! Vous etes jeune"
+
