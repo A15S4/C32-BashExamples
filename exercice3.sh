472ee5cf06053200e4592ca99901ce8d69d8da20
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 12:22:20 -0400

    Création exercice3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+
+#EXERCICE FAIRE UNE BOUCLE QUI AFFICHE UNE BOMBE EN AUGMENTANT LES ESPACES A CHAQUE FOIS
+faireEspaces(){
+    #1 = Nombre d'espaces à faire
+    for ((i = 0; i < $1; i++))
+    do
+        echo -n " "
+    done
+}
+
+for((i=0; i < 10; i++))
+do
+    clear
+    faireEspaces $i
+    if [[ $i -lt 9 ]]
+    then
+        echo "@"
+    else
+        echo "BOOM"
+    fi
+    sleep 0.1
+done
