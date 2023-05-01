1278689994096055ef09ec4d6a459307151aa150
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 10:57:36 -0400

    Création 07-fonctions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+maFonction(){
+    echo "param : " $1
+}
+
+maFonction "salut"
+maFonction "toi"
+
+# $0 donne le nom du script si c'est un script et le nom de la fonction si c'est une fonction
+# $1, $2 donne le premier parametre, deuxieme, etc. de quand la fonction est appelée
