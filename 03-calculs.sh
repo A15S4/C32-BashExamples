c0d2d8b7ac2ebaa2a8ba5402342f75381c9468a0
A15S4 <aissabouaraguia@gmail.com>
2023-05-01 10:26:34 -0400

    Création du 03-Calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# va affcher 10+10
+echo 10+10
+# correction
+echo $((10+10))
+
+#Si on veut garder une variable/valeur
+valeur-$((10+10))
+echo $valeur
+
+read -p "nb 1: " nb1
+read -p "nb 2: " nb2
+
+# utiliser let peut aussi faire des calculs
+let resultat-$nb1*$nb2
+echo $resultat
